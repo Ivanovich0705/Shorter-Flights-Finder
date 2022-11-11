a94f03df0f8198364bab61184e6c5179ea6fc992
Ivanovich0705 <ivantenis1@icloud.com>
2022-11-11 00:28:27 -0500

    build(): show flights

--- src/components/chart.vue
+++ src/components/chart.vue
@@ -40,7 +40,6 @@ export default {
       
   },
   mounted() {
-        
 
         console.log("Chart Loaded");
         
@@ -56,7 +55,8 @@ export default {
                    
              var path = d3.geoPath().projection(projection)
              
-             var graphJson = d3.json('https://acomplex-tf-api.herokuapp.com/dijkstra/1146/1147')
+             //var graphJson = d3.json('https://acomplex-tf-api.herokuapp.com/dijkstra/1146/1147')
+             var graphJson = d3.json('https://acomplex-tf-api.herokuapp.com/nodes')
              //var graphJson = d3.json('http://127.0.0.1:5000')
              //var graphJson = d3.json('http://127.0.0.1:5000/nodes')
              var continentalUsJson=d3.json("https://raw.githubusercontent.com/Ivanovich0705/algorith_complex_datasets/main/continental-us.json")
@@ -78,7 +78,8 @@ export default {
                 var promises = [];
 
                 files.forEach(function (url) {
-                    promises.push(d3.json('https://acomplex-tf-api.herokuapp.com/dijkstra/1146/1147'))
+                    //promises.push(d3.json('https://acomplex-tf-api.herokuapp.com/dijkstra/1146/1147'))
+                    promises.push(d3.json('https://acomplex-tf-api.herokuapp.com/nodes'))
                     //promises.push(d3.json('http://127.0.0.1:5000/nodes'))
                     //promises.push(d3.json("/graph.json"))
                     //promises.push(d3.json("https://raw.githubusercontent.com/Ivanovich0705/algorith_complex_datasets/main/continental-us.json"));
